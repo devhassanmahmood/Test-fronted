d0f742df893f332d75f3a918b4244a6d74010a36
github username <github email address>
2018-06-26 10:00:58 -0400

    File Structure

---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Test extends React.Component {
+  render () {
+    return (
+      <div>
+        <img src="https://media.giphy.com/media/4ZYcQtaZvrsl2/giphy.gif" />
+      </div>
+    )
+  }
+}
+
+export default Test;

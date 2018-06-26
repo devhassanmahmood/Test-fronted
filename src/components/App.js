a22751589179766a66fb518d4ef569023c8ee979
github username <github email address>
2018-06-26 09:57:07 -0400

    module structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import '../assets/css/App.css';
 import React, { Component } from 'react';
+import Test from './test';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, Electron!</h1>
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+        <h1>This is React on Electron!</h1>
+      <Test />
       </div>
     );
   }

1238c5459c0a0d163d6ced976d68462a342de8df
github username <github email address>
2018-07-02 14:41:23 -0400

    Starting weather

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 import VideoContainer from './VideoContainer';
 import Clock from './Clock';
 
+
 class App extends Component {
   render() {
     return (
       <div>
       <Clock />
-        <VideoContainer />
+      <VideoContainer />
       </div>
     );
   }

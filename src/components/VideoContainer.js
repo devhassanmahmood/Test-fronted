3d44271b78904ed92c465ee85f72751247ec7cf5
shawncodes <shawn@huthunt.com>
2019-04-01 10:55:48 -0400

    edited api key

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import YTSearch from 'youtube-api-search';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
-const YTAPI = 'AIzaSyA6rrXdudcq423GmY7IH8rRg4tevWYVp_Q'
+const YTAPI = 'AIzaSyAWa1N-uu9aabfxL517qkF3p0NURSGnCWk'
 
 class VideoContainer extends React.Component {
   constructor(props) {
